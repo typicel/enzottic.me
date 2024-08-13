678fb776227069911d0036d6057b01e9fbf6c7f2
typicel <tyler.mccormick@colorado.edu>
2024-08-12 19:20:55 -0600

    add manifest, favicon, and remove unneeded dependencies

--- Package.swift
+++ Package.swift
@@ -10,8 +10,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-        .package(url: "https://github.com/johnsundell/ink.git", from: "0.1.0"),
-//        .package(url: "https://github.com/sliemeobn/elementary.git", from: "0.1.2"),
         .package(url: "https://github.com/pointfreeco/swift-html", from: "0.4.0"),
         .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
     ],
@@ -22,8 +20,6 @@ let package = Package(
             name: "enzottic.me.m",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Ink", package: "ink"),
-//                .product(name: "Elementary", package: "elementary"),
                 .product(name: "Html", package: "swift-html"),
                 .product(name: "Markdown", package: "swift-markdown")
             ]
